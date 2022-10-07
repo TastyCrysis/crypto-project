2babbdb20430cb843ecd667d943d23f1ec20bd74
Philipp Gampe <phil@MacBook-Air.local>
2022-10-07 11:53:59 +0200

    Trending and Style

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,9 +1,12 @@
 import React from "react";
+import CoinSearch from "../components/CoinSearch";
+import Trending from "../components/Trending";
 
-const Home = () => {
+const Home = ({ coins }) => {
   return (
     <div>
-      <div></div>
+      <CoinSearch coins={coins} />
+      <Trending />
     </div>
   );
 };
